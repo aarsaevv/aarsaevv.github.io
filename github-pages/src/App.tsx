7773ed6664e04324c52cc76fde525b4311e976e6
Anna Kautts <annakautts@gmail.com>
2023-02-05 22:56:29 +0600

    sass variables, font face

--- github-pages/src/App.tsx
+++ github-pages/src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
 import './App.sass';
 
 function App() {
   return (
-    <div className=''>
+    <div className='container'>
+      <div className="home">
+        <div className="navigation">
+          <div className="navigation__logo"></div>
+          <div className="navigation__button">sklfngkdfnhklzdfhnkszdnh</div>
+        </div>
+      </div>
     </div>
   );
 }
--- github-pages/src/index.tsx
+++ github-pages/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'normalize.css'
 import './index.sass';
 import App from './App';
 
